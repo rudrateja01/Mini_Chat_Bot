5def3e7461a66ad1e1ce4f5e4ed6e73ac793f3da
rudrateja01 <rudrateja8897@gmail.com>
2025-12-03 22:26:03 +0530

    first commit

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/chatbox")
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{console.log("Database Connected")})
 .catch((error)=>{console.log(error.message)});
